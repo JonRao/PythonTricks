5ee1e0550f8885dad2a0e2b888038c70e527eb33
Jon Rao <raozijian@gmail.com>
2016-04-14 23:15:28 -0400

    from my mac

--- UpdatePip.py
+++ UpdatePip.py
@@ -2,4 +2,4 @@ import pip
 from subprocess import call
 
 for dist in pip.get_installed_distributions():
-    call("pip install --upgrade " + dist.project_name, shell=True)
+    call("sudo -H pip install --upgrade " + dist.project_name, shell=True)
